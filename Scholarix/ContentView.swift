868e680b10119d300466c15baa56ece8f5a5412b
Sahaj Mandalia <sahajmandalia@icloud.com>
2025-10-12 17:20:36 -0400

    Initial Commit

---
+++ Scholarix/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Scholarix
+//
+//  Created by Kunal Mandalia on 10/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Scholarix/ScholarixApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScholarixApp.swift
+//  Scholarix
+//
+//  Created by Kunal Mandalia on 10/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ScholarixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
