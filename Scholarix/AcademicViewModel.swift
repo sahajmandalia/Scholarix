81564f5557d9d92ca10573b75ba2f4bd54f50376
Sahaj Mandalia <sahajmandalia@icloud.com>
2025-12-07 11:57:55 -0500

    NW - TestFlight Today

---
+++ Scholarix/AcademicViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AcademicViewModel.swift
+//  Scholarix
+//
+//  Created by Kunal Mandalia on 12/6/25.
+//
+
+import Foundation
---
+++ Scholarix/Theme.swift
@@ -0,0 +1,8 @@
+//
+//  Theme.swift
+//  Scholarix
+//
+//  Created by Kunal Mandalia on 12/6/25.
+//
+
+import Foundation
