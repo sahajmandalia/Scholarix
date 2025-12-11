6acd773252c72bcc22c6159cfa450171aed5c92a
Sahaj Mandalia <sahajmandalia@icloud.com>
2025-12-10 21:33:56 -0500

    ready for internal

---
+++ Scholarix/UtilityViews.swift
@@ -0,0 +1,18 @@
+//
+//  UtilityViews.swift
+//  Scholarix
+//
+//  Created by Sahaj Mandalia on 12/10/25.
+//
+
+import SwiftUI
+
+struct UtilityViews: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UtilityViews()
+}
